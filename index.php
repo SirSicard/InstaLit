d7921713a9b7ed106f6774eb70514414ba278817
Ashish <ashisharyal64@gmail.com>
2021-02-02 12:46:21 +0100

    initial commit

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 // this is where it all begans............by making people sign up for the app.
+// a login form with conditional action buttons
 echo "you have pulled the instaLIT successfully, now check for folder to see if they got pulled right or not!";
