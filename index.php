6ebfb7cea8a48f1d9cbc3c2dc857110b79d90177
Mattias Herzig <Mattiasherzig@hotmail.com>
2021-02-04 10:15:15 +0100

    Restructured base template
    Restructured and added more template folders and files

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php
-// this is where it all begans............by making people sign up for the app.
-// a login form with conditional action buttons
-echo "you have pulled the instaLIT successfully, now check for folder to see if they got pulled right or not!";
+<?php include("includes/footer.php"); ?>
+<?php include("includes/footer.php"); ?>
+<body>
+
+
+</body>
