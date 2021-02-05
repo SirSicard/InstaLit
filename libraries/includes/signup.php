c29dad59e79a89b565efdb78c4177a6d9d748826
Nikola <nikola.tomasovic@medieinstitutet.se>
2021-02-05 17:37:23 +0100

    made the signup form
    signup

---
+++ libraries/includes/signup.php
@@ -0,0 +1,14 @@
+<form method="post" action="">
+                    <div class="form-group">
+                        <label class="form-label" for="email">email</label>
+                        <input class="form-input" id="email" name="email" type="email" required>
+                        <label class="form-label" for="fullname">Fullname</label>
+                        <input class="form-input" id="fullname" name="fullname" type="fullname" required>
+                        <label class="form-label" for="username">Username</label>
+                        <input class="form-input" id="username" name="username" type="username" required>
+                        <label class="form-label" for="password">Lösenord</label>
+                        <input class="form-input" id="password" name="password" type="password" required>
+                        </label>
+                        <input type="submit" class="" value="Create account">
+                    </div>
+                </form>
