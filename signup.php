9609ecb0d06df87927a070546ebc642f3a14d0f5
Nikola <nikola.tomasovic@medieinstitutet.se>
2021-02-05 17:43:23 +0100

    created signup.php
    signup form

--- signup.php
+++ signup.php
@@ -1,14 +1,19 @@
+<?php
+include_once("libraries/includes/header.php");
+?>
 <form method="post" action="">
-                    <div class="form-group">
-                        <label class="form-label" for="email">email</label>
-                        <input class="form-input" id="email" name="email" type="email" required>
-                        <label class="form-label" for="fullname">Fullname</label>
-                        <input class="form-input" id="fullname" name="fullname" type="fullname" required>
-                        <label class="form-label" for="username">Username</label>
-                        <input class="form-input" id="username" name="username" type="username" required>
-                        <label class="form-label" for="password">Lösenord</label>
-                        <input class="form-input" id="password" name="password" type="password" required>
-                        </label>
-                        <input type="submit" class="" value="Create account">
-                    </div>
-                </form>
+    <div class="form-group">
+        <label class="form-label" for="email">email</label>
+        <input class="form-input" id="email" name="email" type="email" required>
+        <label class="form-label" for="fullname">Fullname</label>
+        <input class="form-input" id="fullname" name="fullname" type="fullname" required>
+        <label class="form-label" for="username">Username</label>
+        <input class="form-input" id="username" name="username" type="username" required>
+        <label class="form-label" for="password">Lösenord</label>
+        <input class="form-input" id="password" name="password" type="password" required>
+        </label>
+        <input type="submit" class="" value="Create account">
+    </div>
+</form>
+
+<?php include_once("libraries/includes/footer.php"); ?>
