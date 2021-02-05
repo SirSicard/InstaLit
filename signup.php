bb1e0209c1fe70b3f931ebdf68f64088eab16adb
Nikola <nikola.tomasovic@medieinstitutet.se>
2021-02-05 18:58:48 +0100

    edited signup
    edited signup form

--- signup.php
+++ signup.php
@@ -1,19 +1,36 @@
 <?php
 include_once("libraries/includes/header.php");
 ?>
-<form method="post" action="">
-    <div class="form-group">
-        <label class="form-label" for="email">email</label>
-        <input class="form-input" id="email" name="email" type="email" required>
-        <label class="form-label" for="fullname">Fullname</label>
-        <input class="form-input" id="fullname" name="fullname" type="fullname" required>
-        <label class="form-label" for="username">Username</label>
-        <input class="form-input" id="username" name="username" type="username" required>
-        <label class="form-label" for="password">Lösenord</label>
-        <input class="form-input" id="password" name="password" type="password" required>
-        </label>
-        <input type="submit" class="" value="Create account">
+
+<div class="body-container">
+<div class="content-container">
+<div class="card text-center" style="width: 18rem;">
+    <img src="images/ui/logoXL.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Create an account</h5>
+        <form method="post" action="">
+            <div class="form-group">
+                <input class="form-input" id="email" name="email" type="email" placeholder="Email" required>
+                <input class="form-input" id="fullname" name="fullname" type="fullname" placeholder="Fullname" required>
+                <input class="form-input" id="username" name="username" type="username" placeholder="Username" required>
+                <input class="form-input" id="password" name="password" type="password" placeholder="Password" required>
+                <input type="submit" class="" value="Create account">
+            </div>
+        </form>
     </div>
-</form>
+</div>
+</div>
+</div>
+
+<div class="content-container">
+<div class="card text-center" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Already have an account?</h5>
+    <a href="login.php">Log in</a>
+  </div>
+  
+</div>
+</div>
+
 
 <?php include_once("libraries/includes/footer.php"); ?>
