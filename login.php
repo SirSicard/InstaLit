7e4847ba6417c2b2ef68bb227926388d40000315
Mattias Herzig <Mattiasherzig@hotmail.com>
2021-02-08 13:57:35 +0100

    changed header in login and signup
    added style

--- login.php
+++ login.php
@@ -1,9 +1,27 @@
-<?php
-include_once("libraries/includes/header.php");
-?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- Font Awesome Script -->
+    <script src="https://kit.fontawesome.com/1f88de1726.js" crossorigin="anonymous"></script>
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="libraries/css/style.css">
+    <link rel="stylesheet" href="libraries/css/login.css">
+    
+    <title>InstaLIT</title>
+</head>
+<body>
+<body>
+
 <div class="center">
   <div class="container">
-    <div class="text">Login Form</div>
+    <img src="images/ui/logoXL.png" alt="" class="login-img">
+    <div class="text"></div>
     <form action="#">
       <div class="data">
         <label>Email or Phone</label>
@@ -19,9 +37,10 @@ include_once("libraries/includes/header.php");
         <div class="inner"></div>
       <button type="submit">login</button>
       </div>
-      <div class="signup-link">Not a member? <a href="#">Signup now</a></div>
+      <div class="signup-link">Not a member? <a href="signup.php">Signup now</a></div>
     </form>
   </div>
 </div>
-
-<?php include_once("libraries/includes/footer.php"); ?>
+  
+</body>
+</html>
