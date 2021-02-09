37ac4c01f3240f086d7d059ce8355d5e0407572d
Mattias Herzig <Mattiasherzig@hotmail.com>
2021-02-09 15:44:35 +0100

    padding changes
    .

--- editprofile.php
+++ editprofile.php
@@ -15,13 +15,14 @@
                     <h4 class="text-right">Profile Settings</h4>
                 </div>
                 <div class="row mt-2">
-                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-                    <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control" value="" placeholder="Last Name"></div>
+                    <div class="col-md-6 py-1"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
+                    <div class="col-md-6 py-1"><label class="labels">Last Name</label><input type="text" class="form-control" value="" placeholder="Last Name"></div>
+                    <div class="col-md-12 py-1"><label class="labels">Occupation</label><input type="text" class="form-control" placeholder="enter your phone number" value=""></div>
                 </div>
                 <div class="row mt12">
-                    <div class="col-md-12"><label class="labels">Phone number</label><input type="text" class="form-control" placeholder="enter your phone number" value=""></div>
-                    <div class="col-md-12"><label class="labels">Address</label><input type="text" class="form-control" placeholder="Address" value=""></div>
-                    <div class="col-md-12"><label class="labels">Email</label><input type="text" class="form-control" placeholder="Enter your email" value=""></div>
+                    <div class="col-md-12 py-1"><label class="labels">Phone number</label><input type="text" class="form-control" placeholder="enter your phone number" value=""></div>
+                    <div class="col-md-12 py-1"><label class="labels">Address</label><input type="text" class="form-control" placeholder="Address" value=""></div>
+                    <div class="col-md-12 py-1"><label class="labels">Email</label><input type="text" class="form-control" placeholder="Enter your email" value=""></div>
                 </div>
                 <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
             </div>
