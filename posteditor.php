553fc3cbf4a429730ffb7c8b8befc05559db674b
visi <visiface@live.com>
2021-02-10 11:06:42 +0100

    Skeleton for posteditor.php

--- posteditor.php
+++ posteditor.php
@@ -1,3 +1,34 @@
 <?php include("libraries/includes/header.php"); ?>
 
-<?php include("libraries/includes/footer.php"); ?>
+<div class="row">
+  <div class="col-12">
+    <div class="post-container">
+      <div class="card">
+        <div class="card-header">
+          <div class="content-container">
+            <div>
+              <h4>Compose Your Post</h4>
+            </div>
+          </div>
+          
+        </div>
+        <div class="card-body">
+
+          <form method="post">
+
+            <h6>Upload an Image</h6>
+              <input type="file">
+            
+            <h6>Type a Caption</h6>
+              <textarea name="comment" rows="4" cols="50"></textarea>
+          </form>
+          
+        
+        <div class="card-footer text-muted text-center">
+          submit
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php include("libraries/includes/footer.php"); ?>
