7ff6c19264f7a58d542b554623037c1f6ded0445
Mattias Herzig <Mattiasherzig@hotmail.com>
2021-02-10 12:25:08 +0100

    rehaul of edit profile page

---
+++ dummyprofileedit.php
@@ -0,0 +1,33 @@
+
+
+<div class="col-md-8 border-right">
+            <div class="p-3 py-5">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h4 class="text-right">Profile Settings</h4>
+                </div>
+                <div class="row mt-2">
+                    <div class="col-md-6 py-1"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
+                    <div class="col-md-6 py-1"><label class="labels">Last Name</label><input type="text" class="form-control" value="" placeholder="Last Name"></div>
+                    <div class="col-md-12 py-1"><label class="labels">Occupation</label><input type="text" class="form-control" placeholder="enter your phone number" value=""></div>
+                </div>
+                <div class="row mt12">
+                    <div class="col-md-12 py-1"><label class="labels">Phone number</label><input type="text" class="form-control" placeholder="enter your phone number" value=""></div>
+                    <div class="col-md-12 py-1"><label class="labels">Address</label><input type="text" class="form-control" placeholder="Address" value=""></div>
+                    <div class="col-md-12 py-1"><label class="labels">Email</label><input type="text" class="form-control" placeholder="Enter your email" value=""></div>
+                </div>
+                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+            </div>
+            <div class="col-md-12 border-right">
+            <div class="p-3 py-5">
+                <div class="d-flex justify-content-center align-items-center mb-3">
+                    <h4 class="text-right">Change Password</h4>
+                </div>
+                <div class="row mt12">
+                    <div class="col-md-12"><label class="labels">Enter your new password here</label><input type="text" class="form-control" placeholder="Password" value=""></div>
+                </div>
+                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save your new password</button></div>
+            </div>
+        </div>
+</div>
+        
+<?php include("libraries/includes/footer.php"); ?>
--- editprofile.php
+++ editprofile.php
@@ -1,51 +1,97 @@
 <?php include("libraries/includes/header.php"); ?>
 
-<div class="edit-container rounded bg-white mt-5 mb-5">
-    <div class="row">
-        <div class="col-md-3 border-right">
-            <div class="card">
-                <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-                    <img class="rounded-circle mt-5" width="150px" src="https://scontent.fmmx3-1.fna.fbcdn.net/v/t1.0-9/118516678_10157156522332471_3673352244740208855_o.jpg?_nc_cat=103&ccb=2&_nc_sid=84a396&_nc_ohc=ZWx0eczviwIAX_Pd0Tj&_nc_ht=scontent.fmmx3-1.fna&oh=5dc1134ff6e622e511d8c82b4f95094b&oe=604A0696">
-                    <span class="font-weight-bold">Mattias Herzig</span>
-                    <span class="text-black-50">mattias.herzig@medieinstitutet.se</span>
+<div class="profile-container">
+    <div class="main-body">
+        <div class="row gutters-sm">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-column align-items-center text-center">
+                            <img class="rounded-circle mt-5" width="150px" src="https://scontent.fmmx3-1.fna.fbcdn.net/v/t1.0-9/118516678_10157156522332471_3673352244740208855_o.jpg?_nc_cat=103&ccb=2&_nc_sid=84a396&_nc_ohc=ZWx0eczviwIAX_Pd0Tj&_nc_ht=scontent.fmmx3-1.fna&oh=5dc1134ff6e622e511d8c82b4f95094b&oe=604A0696">
+                            <span class=""><b>Mattias Herzig</b></span>
+                            <span class="text-secondary mb-1">Full Stack Developer</span>
+                            <span class="text-secondary mb-1">Malmö, Sweden</span>
+                            <span class="text-secondary mb-1">mattias.herzig@medieinstitutet.se</span>
+                        </div>
+                        <div class="d-flex flex-column align-items-center text-center pt-5" id="img-section"> <b>Profile Photo</b>
+                            <p>Accepted file type .png. Less than 1MB</p> <button class="btn btn-primary profile-button"><b>Upload</b></button>
+                        </div>
+                    </div>
                 </div>
-            <div class="d-flex flex-column align-items-center text-center" id="img-section"> <b>Profile Photo</b>
-                <p>Accepted file type .png. Less than 1MB</p> <button class="btn btn-primary profile-button"><b>Upload</b></button>
+                <div class="card mt-3">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe mr-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
+                            <span class="text-secondary">https://mattiasherzig.com</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github mr-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
+                            <span class="text-secondary">mattiasherzig</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
+                            <span class="text-secondary">@mattiasherzig</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
+                            <span class="text-secondary">mattiasherzig</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
+                            <span class="text-secondary">mattiasherzig</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-8 card">
+                <div class="p-3 py-5">
+                    <div class="d-flex flex-column align-items-center text-center">
+                        <h4 class="">Profile Settings</h4>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col-md-6 py-1">
+                            <label class="labels">Name</label>
+                            <input type="text" class="form-control" placeholder="first name" value="">
+                        </div>
+                        <div class="col-md-6 py-1">
+                            <label class="labels">Last Name</label>
+                            <input type="text" class="form-control" value="" placeholder="Last Name">
+                        </div>
+                        <div class="col-md-12 py-1"><label class="labels">Occupation</label><input type="text" class="form-control" placeholder="enter your phone number" value=""></div>
+                    </div>
+                    <div class="row mt12">
+                        <div class="col-md-12 py-1">
+                            <label class="labels">Phone number</label>
+                            <input type="text" class="form-control" placeholder="enter your phone number" value="">
+                        </div>
+                        <div class="col-md-12 py-1">
+                            <label class="labels">Address</label>
+                            <input type="text" class="form-control" placeholder="Address" value="">
+                        </div>
+                        <div class="col-md-12 py-1">
+                            <label class="labels">Email</label>
+                            <input type="text" class="form-control" placeholder="Enter your email" value="">
+                        </div>
+                    </div>
+                    <div class="mt-5 text-center">
+                        <button class="btn btn-primary profile-button" type="button">Save Profile</button>
+                    </div>
+                    <div class="row mt12 py-4">
+                        <div class="col-md-12">
+                            <label class="labels">Change your password</label>
+                            <input type="password" class="form-control" placeholder="enter your new password" value="">
+                        </div>
+                    </div>
+                    <div class="mt-1 text-center">
+                        <button class="btn btn-primary profile-button" type="button">Save Your new password</button>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-        <div class="col-md-8 border-right">
-            <div class="p-3 py-5">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                    <h4 class="text-right">Profile Settings</h4>
-                </div>
-                <div class="row mt-2">
-                    <div class="col-md-6 py-1"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-                    <div class="col-md-6 py-1"><label class="labels">Last Name</label><input type="text" class="form-control" value="" placeholder="Last Name"></div>
-                    <div class="col-md-12 py-1"><label class="labels">Occupation</label><input type="text" class="form-control" placeholder="enter your phone number" value=""></div>
-                </div>
-                <div class="row mt12">
-                    <div class="col-md-12 py-1"><label class="labels">Phone number</label><input type="text" class="form-control" placeholder="enter your phone number" value=""></div>
-                    <div class="col-md-12 py-1"><label class="labels">Address</label><input type="text" class="form-control" placeholder="Address" value=""></div>
-                    <div class="col-md-12 py-1"><label class="labels">Email</label><input type="text" class="form-control" placeholder="Enter your email" value=""></div>
-                </div>
-                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
-            </div>
-            <div class="col-md-12 border-right">
-            <div class="p-3 py-5">
-                <div class="d-flex justify-content-center align-items-center mb-3">
-                    <h4 class="text-right">Change Password</h4>
-                </div>
-                <div class="row mt12">
-                    <div class="col-md-12"><label class="labels">Enter your new password here</label><input type="text" class="form-control" placeholder="Password" value=""></div>
-                </div>
-                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save your new password</button></div>
-            </div>
-        </div>
-        </div>
-        
-    
     </div>
 </div>
 
-<?php include("libraries/includes/footer.php"); ?>
+<?php include_once("libraries/includes/footer.php"); ?>
+
+
+        
